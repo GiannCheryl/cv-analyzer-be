0e841093d341988578e432612e2d91624b4832aa
GiannChryl <gianinacheryl04@gmail.com>
2026-05-19 04:29:48 +0700

    commit lagi

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { db } = require("../db");
+const authenticate = require("../middleware/auth");
 
 const router = express.Router();
 
